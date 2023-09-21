a925096e5b567b9fab7e4a2682e97f26e57c9115
KasunUmesh <kasunumesh38@gmail.com>
2023-09-21 18:16:10 +0530

    Create TechLead DTO class

---
+++ src/main/java/lk/ijse/gdse/dto/TechLeadDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TechLeadDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String profileIMG;
+}
